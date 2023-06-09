e77906e6c66240e126c10200c21374d413f96c98
6Jones101 <131403206+6Jones101@users.noreply.github.com>
2023-06-09 18:11:01 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 -- JOIN FOR SUPPORT https://discord.gg/qRQ5f5rdmN
 Config = {
     Locations = {
-        { name = "Moneywash", x = 1136.79, y = -989.22, z = 46.11, tax = 10 }, -- this is the location from the one on the fourms post!
-        { name = "Location 2", x = 1234.56, y = -789.01, z = 32.10, tax = 15 },
-        { name = "Location 3", x = 987.65, y = -543.21, z = 12.34, tax = 20 },
+        { name = "Moneywash", x = 1136.79, y = -989.22, z = 46.11, tax = 30 }, -- this is the location from the one on the forums post!
+        { name = "Location 2", x = -1080.32, y = -1677.51, z = 4.83, tax = 20 },
+        { name = "Location 3", x = 1537.59, y = 6340.8, z = 24.11, tax = 10 },
     },
 }
 
